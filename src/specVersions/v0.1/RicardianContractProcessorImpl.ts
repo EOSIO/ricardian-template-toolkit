92d8956fa75effb573586b991db6dcb4cd730eb6
Eddie Sheffield <eddie.sheffield@block.one>
2019-06-10 13:44:56 -0400

    Removed deprecated helpers since they were never officially documented as part of the spec before v0.2 anyway.

--- src/specVersions/v0.1/RicardianContractProcessorImpl.ts
+++ src/specVersions/v0.1/RicardianContractProcessorImpl.ts
@@ -1,4 +1,3 @@
-import { RicardianContractRenderError } from '../../RicardianContractRenderError'
 import { RicardianContractProcessorImpl as RCP_0_0 } from '../v0.0/RicardianContractProcessorImpl'
 import { parseAsset } from './helpers'
 
@@ -11,28 +10,8 @@ export class RicardianContractProcessorImpl extends RCP_0_0 {
   constructor() {
     super()
 
-    this.registerWrappedHelper('account_in_permission_level', (permissionLevel: any): string => {
-      if (permissionLevel.actor) {
-        return permissionLevel.actor
-      }
-
-      throw new RicardianContractRenderError(`No 'actor' found in given permission_level`)
-    })
-
-    this.registerWrappedHelper('permission_in_permission_level', (permissionLevel: any): string => {
-      if (permissionLevel.permission) {
-        return permissionLevel.permission
-      }
-
-      throw new RicardianContractRenderError(`No 'permission' found in given permission_level`)
-    })
-
     this.registerWrappedHelper('amount_from_asset', (asset: string): string => {
       return parseAsset(asset).amount
-    })
-
-    this.registerWrappedHelper('symbol_name_from_asset', (asset: string): string => {
-      return parseAsset(asset).symbol
     })
   }
 
